7429a2306dbea606532f5d1301e5d808bf1eefab
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-10 11:41:25 +0530

    Generated: `react-script-maker-website/src/components/Header.js`

---
+++ react-script-maker-website/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Scripts</li>
+          <li>Login</li>
+          <li>Register</li>
+          <li>User Dashboard</li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
